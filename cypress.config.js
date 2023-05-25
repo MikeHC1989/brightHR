d7f124a470bca197cce3171fefd49e415a47abef
MikeHC1989 <michealheeney@gmail.com>
2023-05-25 14:06:26 +0100

    ok

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       on('file:preprocessor', cucumber())
     },
-    specPattern: 'cypress/e2e/stepDefinition/*.feature',
+    specPattern: 'cypress/e2e/*.feature',
   },
 });
