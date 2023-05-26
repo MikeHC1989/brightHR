0044cfab26cdc988bc9651e31b6d7d6e5a64e003
MikeHC1989 <micheal.heeney@gmail.com>
2023-05-26 09:17:39 +0100

    ok

--- cypress/e2e/stepDefinition/addEmployee.js
+++ cypress/e2e/stepDefinition/addEmployee.js
@@ -1,13 +1,13 @@
 import {Given, When, Then} from 'cypress-cucumber-preprocessor/steps'
 const employeeData = require('../../fixtures/addEmployee.json');
-
+const loginData = require ('../../fixtures/loginDetails.json')
 
 Given("bright hr is loaded and logged in", ()=> {
     cy.visit('https://sandbox-app.brighthr.com/lite');
     cy.get(".bg-white").click();
     cy.get('.p-5', {timeout: 20000}).should('be.visible');
-    cy.get("#email").type('michealheeney@gmail.com'); 
-    cy.get("#password").type("caleb*CARTWRIGHT*2020");
+    cy.get("#email").type(loginData.correctDetails.emailAddress); 
+    cy.get("#password").type(loginData.correctDetails.password);
     cy.get(".bg-accent-500").click();
     cy.get(':nth-child(5) > .rounded', {timeout: 20000}).should('be.visible').then((displayed) =>{
             if (displayed){cy.get(':nth-child(5) > .rounded').click()
@@ -26,17 +26,17 @@ Given("bright hr is loaded and logged in", ()=> {
 
     });
 
-Given("an employee is added", (employeeData)=> {
+Given("an employee is added", ()=> {
     cy.get('.active > .h-16 > .relative').click();
     cy.get('.my-6 > .rounded', {timeout: 20000}).should('be.visible');
     cy.get('.my-6 > .rounded').click();
     cy.get('form > :nth-child(1)',{timeout: 20000}).should('be.visible');
     //employee details
-    cy.get('#firstName').type("Helena");
-    cy.get('#lastName').type("Jones");
-    cy.get('#email').type("testing2@test.com");
-    cy.get('#phoneNumber').type("0786999999");
-    cy.get('#jobTitle').type("Test2");
+    cy.get('#firstName').type(employeeData.employee1.firstname);
+    cy.get('#lastName').type(employeeData.employee1.surname);
+    cy.get('#email').type(employeeData.employee1.email);
+    cy.get('#phoneNumber').type(employeeData.employee1.phonenumber);
+    cy.get('#jobTitle').type(employeeData.employee1.jobtitle);
     cy.get('.sc-kgoBCf').click();
        
     //datepicker
@@ -48,16 +48,16 @@ Given("an employee is added", (employeeData)=> {
     cy.wait('@employeeAdded').its('response.statusCode').should('eq',201);
     
 });
-When("another employee is added", (employeeData)=> {
+When("another employee is added", ()=> {
     cy.get('.bg-gray-200 .bg-accent-500',{timeout: 100000}).should('be.visible');
     cy.get('.bg-gray-200 .bg-accent-500').click();
     cy.get('form > :nth-child(1)',{timeout: 20000}).should('be.visible');
     //employee details
-    cy.get('#firstName').type("Joe");
-    cy.get('#lastName').type("Bloggs");
-    cy.get('#email').type("testing@test.com");
-    cy.get('#phoneNumber').type("07812345678");
-    cy.get('#jobTitle').type("test");
+    cy.get('#firstName').type(employeeData.employee2.firstname);
+    cy.get('#lastName').type(employeeData.employee2.surname);
+    cy.get('#email').type(employeeData.employee2.email);
+    cy.get('#phoneNumber').type(employeeData.employee2.phonenumber);
+    cy.get('#jobTitle').type(employeeData.employee2.jobtitle);
 
     //datepicker
     cy.get('[data-testid="input-selector"]').click();
@@ -71,20 +71,26 @@ When("another employee is added", (employeeData)=> {
     
 });
 
-When("navigate back to employee tab", (employeeData)=> {
+When("navigate back to employee tab", ()=> {
     cy.get('.active > .h-16 > .relative').click();
     cy.get('.sc-LKuAh', {timeout: 20000}).should('be.visible');
     
 });
-Then("both employees are displayed", (employeeData)=> {
-    cy.get("#app > div > div > div.side-nav > div > div.grid.grid-cols-1.gap-8.py-6.mt-6.xl\:grid-cols-3.md\:grid-cols-2").should('have.value',
+Then("both employees are displayed", ()=> {
+    cy.fixture('addEmployee.json').then((employeeData) => {
+
+        let index = 0;
+        while (index < employeeData.length) {
     
-        cy.contains(("Helena"+" "+"Jones"), {include: 'visible'}),
-        cy.contains("Test2", {include: 'visible'}),
-        cy.contains("HJ", {include: 'visible'}),
-        cy.contains(("Joe"+" "+"Bloggs"), {include: 'visible'}),
-        cy.contains("Test", {include: 'visible'}),
-        cy.contains("JB", {include: 'visible'}),
-    )
-})
+            const record = employeeData;
     
+            cy.contains((record.firstname+" "+record.surname), {include: 'visible'})
+            cy.contains(record.jobtitle, {include: 'visible'})
+            cy.contains(record.initials, {include: 'visible'})
+    
+            index++;
+    
+        }
+    });
+
+        })
